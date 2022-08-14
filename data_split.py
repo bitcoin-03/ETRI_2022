2f16cd6a0ce085c0abd8a52fca6ab02aefb841d0
kdg1993 <daegunkim0425@gmail.com>
2022-08-14 21:16:53 +0900

    [feat] Add splitted csv file and code
    New
    * Simple split code
    * Splitted csv file

--- data_split.py
+++ data_split.py
@@ -6,3 +6,31 @@ Purpose: ETRI dataset csv file train-validation split script.
 Author: Daegun Kim
 """
 
+# Import packages
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+# Custom split functions
+def simple_split(path:str, new_path: str):
+
+    # Load original train set information
+    df = pd.read_csv(path)
+    
+    # Get split
+    df_tr, df_val, _, _ = train_test_split(
+        df, df, test_size=0.2, random_state=42)
+
+    # Change the column 'Split'
+    df.Split.loc[df_val.index] = 'val'
+    
+    # Save as new csv file
+    df.to_csv(new_path)
+    
+
+# Main script
+if __name__=='__main__':
+    path = 'task1_data/info_etri20_emotion_train.csv'
+    new_path = 'task1_data/info_etri20_emotion_tr_val_simple.csv'
+
+    simple_split(path, new_path)
