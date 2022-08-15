848480e5d30a2de1d45a745e63fcdf992be90eb8
Jaeook Jeong <wodnr0710@naver.com>
2022-08-16 00:58:18 +0900

    add checkpoint & change the model class -close #14

--- train.py
+++ train.py
@@ -63,17 +63,24 @@ parser.add_argument(
 parser.add_argument(
     "--seed", default=None, type=int, help="seed for initializing training. "
 )
-# models 안에 저장되는 이름과 wandb 실험 앞에 붙는 이름입니다. 
+# models 안에 저장되는 이름과 wandb 실험 앞에 붙는 이름입니다.
 parser.add_argument("--name", default="exp", help="model save at {exp_num}_모델이름")
 # 실험에 대한 설명입니다.(실험 구분 목적) wandb에 exp_모델이름 뒤에 붙습니다.
-parser.add_argument("--explan", default="", help="experiment description, ex. exp_efficientnet_{explan}")
+parser.add_argument(
+    "--explan", default="", help="experiment description, ex. exp_efficientnet_{explan}"
+)
+# 10에폭마다 체크포인트를 저장하게 구현하였고, 만약 20에폭까지 학습됐고, 이어서 학습하고 싶으시면
+# model_resume_20.pth를 쓰시면 됩니다. 그럼 21에폭부터 학습이 이어집니다.
+parser.add_argument("--resume_from", type=str, default=None, help="model_resume_20.pth")
+
 
 a = parser.parse_args()
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(DEVICE)
 
+
 def increment_path(path, exist_ok=False):
-    """ Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
+    """Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
     Args:
         path (str or pathlib.Path): f"{model_dir}/{args.name}".
         exist_ok (bool): whether increment path (increment if False).
@@ -83,12 +90,11 @@ def increment_path(path, exist_ok=False):
         return f"{path}"
     else:
         dirs = glob.glob(f"{path}*")
-        matches = [
-            re.search(rf"%s(\d+)" % path.stem, d) for d in dirs
-        ]
-        i = [int(m.groups()[0]) for m in matches if m] 
+        matches = [re.search(rf"%s(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]
         n = max(i) + 1 if i else 2
         return f"{path}{n}"
+
 
 def main():
     """The main function for model training."""
@@ -97,15 +103,17 @@ def main():
 
     # save_path = "models/" + a.version
     # models 폴더에 exp, exp1, ....expN 으로 저장됩니다.
-    save_path = increment_path(os.path.join('models/', a.name))
+    save_path = increment_path(os.path.join("models/", a.name))
     if os.path.exists(save_path) is False:
         os.makedirs(save_path)
 
     # 모델은 parser로 network.py에 구현되어 있는 클래스 이름을 입력받아서 생성되게끔 했습니다.
     print("Loading model...")
     # net = EfficientNet_emo().to(DEVICE)
-    if a.model == "Baseline_ResNet_emo": net = Baseline_ResNet_emo.to(DEVICE)
-    elif a.model == "EfficientNet_emo": net = EfficientNet_emo().to(DEVICE)
+    if a.model == "Baseline_ResNet_emo":
+        net = Baseline_ResNet_emo.to(DEVICE)
+    elif a.model == "EfficientNet_emo":
+        net = EfficientNet_emo().to(DEVICE)
 
     print("Loading data....")
     # 경로는 각자 맞춰주시면 될것같습니다.
@@ -115,11 +123,34 @@ def main():
     train_dataset = ETRIDataset_emo(
         df, base_path="../TEAM비뜨코인/ETRI_Season3/task1_data/train/"
     )
+    # train_dataloader = torch.utils.data.DataLoader(
+    #     train_dataset, batch_size=a.batch_size, shuffle=True, num_workers=0
+    # )
+
     train_dataloader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=a.batch_size, shuffle=True, num_workers=0
+        train_dataset,
+        batch_size=a.batch_size,
+        shuffle=True,
+        num_workers=torch.cuda.device_count() * 4,
+        pin_memory=True,
     )
 
-    optimizer = torch.optim.Adam(net.parameters(), lr=a.lr)
+    if a.resume_from:
+        # 저장했던 중간 모델 정보를 읽습니다.
+        path = save_path + "/" + a.resume_from
+        checkpoint = torch.load(path)
+        net.load_state_dict(checkpoint["model_state_dict"])
+
+        optimizer = torch.optim.Adam(net.parameters(), lr=checkpoint["lr"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        resume_epoch = checkpoint["epoch"]
+        epochs = a.epochs
+        loss = checkpoint["loss"]
+    else:
+        optimizer = torch.optim.Adam(net.parameters(), lr=a.lr)
+        epochs = a.epochs
+
+    # optimizer = torch.optim.Adam(net.parameters(), lr=a.lr)
     criterion = nn.CrossEntropyLoss().to(DEVICE)
 
     total_step = len(train_dataloader)
@@ -131,9 +162,14 @@ def main():
     project = "본인 프로젝트 이름", 
     name="실험 이름, default=exp_모델명"
     """
-    increment_name = save_path.split('/')[-1]
+    increment_name = save_path.split("/")[-1]
     model_explan = f"{'_'+a.explan if a.explan else ''}"
-    wandb.init(project="model-test", name=f"{increment_name}_{a.model}{model_explan}", entity="bitcoin-etri", config=a)
+    wandb.init(
+        project="model-test",
+        name=f"{increment_name}_{a.model}{model_explan}",
+        entity="bitcoin-etri",
+        config=a,
+    )
     wandb.save()
     wandb.watch(net)
 
@@ -190,12 +226,25 @@ def main():
             optimizer = torch.optim.Adam(net.parameters(), lr=a.lr)
             print(f"learning rate is decayed... learning rate is {a.lr}")
 
+            # 재학습을 위해 10에포크마다 모델 저장
+            torch.save(
+                {
+                    "epoch": epoch + 1,
+                    "model_state_dict": net.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                    "loss": loss,
+                    "lr": a.lr,
+                },
+                save_path + "/model_resume_" + str(epoch + 1) + ".pth",
+            )
+
         if (epoch + 1) % 20 == 0:
             print("Saving Model....")
             torch.save(
                 net.state_dict(), save_path + "/model_" + str(epoch + 1) + ".pkl"
             )
             print("OK.")
+
         print(
             "Epoch {} is finished. Total Time : {:2.3f} \n".format(
                 epoch + 1, time.time() - t1
